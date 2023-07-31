c5628aa495dc738510abb1b3b29e5e61b987fdc1
isrrael1520 <arellanoisrrael@gmail.com>
2023-07-30 19:43:14 -0500

    login

---
+++ vista/login.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+    <link rel="stylesheet" type="text/css" href="../css/css/all.min.css">
+</head>
+<body>
+    <img class="wave" src="../img/wave.png" alt="">
+    <div class="contenedor">
+        <div class="img">
+            <img src="../img/bg.svg" alt="" >
+        </div>
+        <div class="contenido-login">
+            <form action="">
+                <img src="../img/logo.png" alt="">
+                <h2>Farmacia</h2>
+                <div class="input-div dni">
+                    <div class="i">
+                        <i class="fas fa-user"></i>
+                    </div>
+                    <div class="div">
+                        <h5>DNI</h5>
+                        <input type="text" name="user" class="input">
+                    </div>
+                    
+                </div>
+                <div class="input-div pass"> 
+                    <div class="i">
+                        <i class="fas fa-lock"></i>
+                    </div>
+                    <div class="div">
+                        <h5>Contraseña</h5>
+                        <input type="password" name="pass" class="input">
+                    </div>
+                    
+                </div>
+                <a href="Created Warpiece"></a>
+                <input type="submit" class="btn" value="Iniciar Sesión">
+            </form>
+        </div>
+    </div>
+    <script src="../js/login.js"></script>
+    
+</body>
+</html>
