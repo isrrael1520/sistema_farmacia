504a94a2551801963109a0c211242dae1f505234
isrrael1520 <arellanoisrrael@gmail.com>
2023-08-01 07:10:28 -0500

    Conexion PDO-Acceso a roles-Template

--- vista/login.php
+++ vista/login.php
@@ -5,17 +5,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="../css/style.css">
-    <link rel="stylesheet" type="text/css" href="../css/css/all.min.css">
+    <link rel="stylesheet" type="text/css "href="../css/style.css">
+    <link rel="stylesheet" type="text/css "href="../css/css/all.min.css">
 </head>
+<?php
+session_start();
+if(!empty($_SESSION['us_tipo'])){
+    header('Location: ../controlador/LoginController.php');
+}
+else{
+    session_destroy();
+?>
 <body>
-    <img class="wave" src="../img/wave.png" alt="">
+<img class="wave"src="../img/wave.png" alt="">
     <div class="contenedor">
         <div class="img">
-            <img src="../img/bg.svg" alt="" >
+            <img src="../img/bg.svg" alt="">
         </div>
         <div class="contenido-login">
-            <form action="">
+            <form action="../controlador/LoginController.php" method="post">
                 <img src="../img/logo.png" alt="">
                 <h2>Farmacia</h2>
                 <div class="input-div dni">
@@ -26,24 +34,25 @@
                         <h5>DNI</h5>
                         <input type="text" name="user" class="input">
                     </div>
-                    
                 </div>
-                <div class="input-div pass"> 
+                <div class="input-div pass">
                     <div class="i">
                         <i class="fas fa-lock"></i>
                     </div>
                     <div class="div">
-                        <h5>Contraseña</h5>
+                        <h5>Contrasena</h5>
                         <input type="password" name="pass" class="input">
                     </div>
-                    
                 </div>
-                <a href="Created Warpiece"></a>
-                <input type="submit" class="btn" value="Iniciar Sesión">
+                <a href="../vista/recuperar.php">recuperar password</a>
+                <a href="">Created Warpiece</a>
+                <input type="submit" class="btn" value="iniciar Sesion">
             </form>
         </div>
     </div>
-    <script src="../js/login.js"></script>
-    
 </body>
+<script src="../js/login.js"></script>
 </html>
+<?php
+}
+?>
